aae142ddbe53568d0aed0c4a3d3a28d68bc67fc2
Brendamartins-tech <brenda.tech.ads@gmail.com>
2025-09-21 15:21:28 -0300

    segundo commit

---
+++ Desafio2.js
@@ -0,0 +1,31 @@
+// desafio2.js - Calculadora de Partidas Rankeadas
+
+function calcularNivel(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel = "";
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return { saldoVitorias, nivel };
+}
+
+let vitorias = 85;
+let derrotas = 30;
+
+let resultado = calcularNivel(vitorias, derrotas);
+
+console.log(`O Herói tem de saldo de ${resultado.saldoVitorias}  e está no nível de ${resultado.nivel}`);
