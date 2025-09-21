f796c8bed40fa9794fe9cc6c320718294037b5da
Brendamartins-tech <brenda.tech.ads@gmail.com>
2025-09-21 15:18:34 -0300

    primeiro commit

---
+++ Desafio1.js
@@ -0,0 +1,24 @@
+// Desafio Classificador de Nível de Herói
+let nomeHeroi = "Aragorn";  
+let xpHeroi = 8500;  
+let nivel = "";
+
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
